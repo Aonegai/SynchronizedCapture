a9ded0359aacd7b8a0a691b32025c533d1913e5a
Aonegai <1556337996@qq.com>
2024-05-21 17:16:39 +0800

    commit1

---
+++ Pods/Target Support Files/Pods-SynchronizedCapture-SynchronizedCaptureUITests/Pods-SynchronizedCapture-SynchronizedCaptureUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SynchronizedCapture_SynchronizedCaptureUITests : NSObject
+@end
+@implementation PodsDummy_Pods_SynchronizedCapture_SynchronizedCaptureUITests
+@end
---
+++ Pods/Target Support Files/Pods-SynchronizedCapture/Pods-SynchronizedCapture-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SynchronizedCapture : NSObject
+@end
+@implementation PodsDummy_Pods_SynchronizedCapture
+@end
